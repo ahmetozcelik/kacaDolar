64e46e68ea5c3a0475656d7d19665ac603b73874
Ahmet Ozcelik <ahmetozcelik07@gmail.com>
2019-11-30 14:29:13 +0300

    Initial Commit

---
+++ KacaDolar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KacaDolar
+//
+//  Created by Ahmet Özçelik on 30.11.2019.
+//  Copyright © 2019 Ahmet Özçelik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
